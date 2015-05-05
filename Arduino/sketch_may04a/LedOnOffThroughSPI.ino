4b942886ec1cc3c6a455f41b58be616e9a0956db
Nosion <muhaar@gmail.com>
2015-05-05 15:11:31 +0200

    - SPI kommunikation mellem de 2 arduino boards, en knap til at skukke og tænde lys på over SPI på det andet board.

---
+++ Arduino/sketch_may04a/LedOnOffThroughSPI.ino
@@ -0,0 +1,119 @@
+/*Example taken form http://electronics.stackexchange.com/questions/42197/making-two-arduinos-talk-over-spi 
+and edited by Jakob */
+
+/*
+####### SLAVE ########
+*/
+
+//#include <SPI.h>
+//
+//char buf [100];
+//
+//volatile byte pos;
+//volatile boolean process_it;
+//
+//void setup (void)
+//{
+	//Serial.begin (115200);
+//
+	//// have to send on master in, *slave out*
+	//pinMode(MISO, OUTPUT);
+//
+	//// turn on SPI in slave mode
+	//SPCR |= _BV(SPE);
+//
+	//// get ready for an interrupt
+	//pos = 0;   // buffer empty
+	//process_it = false;
+//
+	//// now turn on interrupts
+	//SPI.attachInterrupt();
+	//
+	//pinMode(7, OUTPUT);	
+//}
+//
+//
+//// SPI interrupt routine
+//ISR (SPI_STC_vect)
+//{
+	//byte c = SPDR;  // grab byte from SPI Data Register
+	//if (pos < sizeof buf)
+	//{
+		//buf [pos++] = c;
+		//if (c == '\n')
+		//process_it = true;
+	//}
+//}
+//
+//void loop (void)
+//{
+	//if (process_it) {
+		//buf [pos] = 0;
+		//Serial.println (buf);
+		//if(buf[0] == 'y') {
+			//digitalWrite(7, HIGH);
+			//pos = 0;
+			//process_it = false;
+		//}	
+	//}
+	//if (process_it) {
+		//buf [pos] = 0;
+		//Serial.println (buf);
+		//if(buf[0] == 'n') {
+			//digitalWrite(7, LOW);
+	//}
+		//pos = 0;
+		//process_it = false;
+	//}
+//}
+
+
+
+/*
+############ MASTER ###########
+*/
+
+#include <SPI.h>
+
+int analogInput0;
+int analogInput1;
+int analogChannel0 = 0;
+int analogChannel1 = 1;
+void setup (void)
+{
+	Serial.begin(115200);
+	digitalWrite(SS, HIGH);
+	SPI.begin ();
+	SPI.setClockDivider(SPI_CLOCK_DIV8);
+}
+
+void loop (void)
+{
+	char c;
+
+	// enable Slave Select
+	digitalWrite(SS, LOW);    // SS is pin 10
+
+	analogInput0 = analogRead(analogChannel0);
+	analogInput1 = analogRead(analogChannel1);
+	
+	if(analogInput0 > 300) {
+		for (const char * p = "y\n" ; c = *p; p++) {
+			SPI.transfer (c);
+			Serial.print(c);
+			delay (500);
+		}
+	}
+	
+	if(analogInput1 > 300) {
+		for (const char * p = "n\n" ; c = *p; p++) {
+			SPI.transfer (c);
+			Serial.print(c);
+			delay (500);
+		}
+	}
+
+	// disable Slave Select
+	digitalWrite(SS, HIGH);
+
+}
